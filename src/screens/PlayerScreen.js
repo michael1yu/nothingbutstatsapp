0597eceae73b04b0c1618d5c796537c2a77dba9c
Michael Yu <michaelyu678@gmail.com>
2019-07-15 01:29:59 -0700

    added stats and player info to player screen

--- src/screens/PlayerScreen.js
+++ src/screens/PlayerScreen.js
@@ -1,8 +1,9 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, SectionList } from "react-native";
 import axios from "axios";
 import ExpandablePanel from "../components/ExpandablePanel";
 import Animated from "react-native-reanimated";
+import { ScrollView } from "react-native-gesture-handler";
 
 const requestStatsUrl = "https://nbaspringboot.herokuapp.com/get_player_stats";
 const requestInfoUrl = "https://nbaspringboot.herokuapp.com/get_player_info";
@@ -75,7 +76,8 @@ class PlayerScreen extends React.Component {
     playerStats = this.state.stats;
 
     return (
-      <View>
+      <ScrollView>
+        <Text>Info</Text>
         <View>
           <Text>Position: {playerInfo.position}</Text>
           <Text>
@@ -83,14 +85,35 @@ class PlayerScreen extends React.Component {
           </Text>
           <Text>Weight: {playerInfo.weight_pounds} lbs</Text>
         </View>
+        <Text>Stats</Text>
         <View>
-          <ExpandablePanel title="test">
-            <Text>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            </Text>
+          <Text>Games Played: {playerStats.games_played}</Text>
+          <Text>Minutes per Game: {playerStats.min}</Text>
+          <ExpandablePanel title={`Points per Game: ${playerStats.pts}`}>
+            <ExpandablePanel title={`Field Goal: ${playerStats.fg_pct}%`}>
+              <Text>Field Goals Made: {playerStats.fgm}</Text>
+              <Text>Field Goals Attempted: {playerStats.fga}</Text>
+            </ExpandablePanel>
+            <ExpandablePanel title={`3 Point: ${playerStats.fg3_pct}%`}>
+              <Text>3 Points Made: {playerStats.fg3m}</Text>
+              <Text>3 Point Attempts: {playerStats.fg3a}</Text>
+            </ExpandablePanel>
+            <ExpandablePanel title={`Free Throw: ${playerStats.fg3_pct}%`}>
+              <Text>3 Points Made: {playerStats.ftm}</Text>
+              <Text>3 Point Attempts: {playerStats.fta}</Text>
+            </ExpandablePanel>
           </ExpandablePanel>
+          <ExpandablePanel title={`Rebounds: ${playerStats.reb}`}>
+            <Text>Offensive Rebounds: {playerStats.oreb}</Text>
+            <Text>Defensive Rebounds: {playerStats.dreb}</Text>
+          </ExpandablePanel>
+          <Text>Assists Per Game: {playerStats.ast}</Text>
+          <Text>Steals Per Game: {playerStats.stl}</Text>
+          <Text>Blocks Per Game: {playerStats.blk}</Text>
+          <Text>Turnovers Per Game: {playerStats.turnover}</Text>
+          <Text>Personal Fouls Per Game: {playerStats.pf}</Text>
         </View>
-      </View>
+      </ScrollView>
     );
   }
 }
